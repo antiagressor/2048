7fc63524497b80fa33ad6376e8e68e59e0992c59
alzolin <alzolin@lab.local>
2024-06-17 15:31:26 +0200

    change title

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Create a title
     const header = document.createElement('div');
     header.className = 'header';
-    header.textContent = 'My Test Project';
+    header.textContent = 'My Test Project_new_version';
 
     // Add the title above game desk
     const gameBoard = document.getElementById('game-board');
