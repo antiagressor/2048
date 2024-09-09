be962c224e93cb557cbcda4e7c476333e4809898
Adm <Adm@DESKTOP-US03O02>
2024-09-09 16:42:39 +0200

    Change Titlr

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Create a title
     const header = document.createElement('div');
     header.className = 'header';
-    header.textContent = 'My Test Project_new_version_09092024';
+    header.textContent = 'My Test Project_new_version2024';
 
     // Add the title above game desk
     const gameBoard = document.getElementById('game-board');
